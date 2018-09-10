dd84fb2d374258a3cd3f8430d1d3b25e414a47e2
nemza4 <nemavholags@gmail.com>
2018-09-10 10:04:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Ionic2RatingModule } from 'ionic2-rating';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
+    Ionic2RatingModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Nav } from 'ionic-angular';
 
+import { LoginPage } from '../login/login'
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+declare var firebase;
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  description;
+  rate;
+  items=[];  
+  comment={
+    description:'',
+    rate:''
   }
 
+  // rate : any = 0;
+
+
+  //  static get parameters() {
+  //   var rating=[];
+  //   return rating;
+  //  }
+
+  // description
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    firebase.database().ref('/comment/').on("value", (snapshot) => {
+      snapshot.forEach((snap) => {
+
+        console.log(snap.val());
+        //append To Item List
+        this.items.push(snap.val());
+
+        return false;
+      });
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+    onModelChange(event){
+      this.rate = event;
+      console.log(event);
+   
+    }
+  //   onSubmit(formData) {
+  //     // let's log our findings
+  //     console.log('Form submission is ', formData);
+  //     var databese = firebase.database();
+  //   databese.ref('/rate/').push(this.rate);
+  //   databese.ref('/rate/').push(this.rate);
+  // }
+  submit(){
+
+    console.log(this.description);
+    this.comment.description = this.description;
+    this.comment.rate = this.rate;
+    var databese = firebase.database();
+    databese.ref('/comment/').push(this.comment);
+    // console.log(this.description)
+  }
+    goToAbout() {
+    this.navCtrl.push("LoginPage");
+  
+  }
 }
+
